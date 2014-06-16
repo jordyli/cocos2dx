8fb8fcc820adb47dbba6d2e8ba88fb23efc31707
Jordy <jordy@manonmoon.com.hk>
2014-06-16 14:50:49 +0800

    Removed sample, scripting and tools folder

--- tools/list_android_mk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-dirs=("Classes" "MSLib" "sqlite")
-for dir in ${dirs[@]}
-do
-    for file in `find $1/$dir -name '*.cpp' -or -name '*.c'`;
-    do
-        echo "$dir/$file \\" | sed -e "s/.*\/\//                   ..\/..\//g"
-    done
-done
